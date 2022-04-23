5d4af2ee83d195d55942586805a28f515e0406b8
Brilliantcanady <venkyrssvv2002@gmail.com>
2022-04-23 19:06:05 +0530

    commit_4

---
+++ app/src/main/java/com/example/tae/User_Bus_List.java
@@ -0,0 +1,93 @@
+package com.example.tae;
+
+import static com.example.tae.User_Login_Page.Uniquecode;
+import static com.example.tae.User_Login_Page.filename;
+
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+public class User_Bus_List extends Fragment {
+
+    private RecyclerView recyclerView;
+    personAdapter adapter; // Create Object of the Adapter class
+    DatabaseReference mbase; // Create object of the
+    SharedPreferences sharedPreferences;
+    String unicodeno;
+    AdapterView.OnItemClickListener     itemClickListener;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.recycleitems, container, false);
+        sharedPreferences=getActivity().getApplicationContext().getSharedPreferences(filename, getActivity().getApplicationContext().MODE_PRIVATE);
+        if(sharedPreferences.contains(Uniquecode)){
+
+            Toast.makeText(getActivity().getApplicationContext(),sharedPreferences.getString(Uniquecode,"") , Toast.LENGTH_SHORT).show();
+        }
+        mbase = FirebaseDatabase.getInstance().getReference().child("bus").child(sharedPreferences.getString(Uniquecode,""));
+
+        recyclerView = view.findViewById(R.id.recycler1);
+        itemClickListener =new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getContext(),"Position : || Value : ",Toast.LENGTH_SHORT).show();
+                Intent iy=new Intent(getContext(),BusTrackMenu.class);
+                startActivity(iy);
+            }
+        };
+        // To display the Recycler view linearly
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(getContext()));
+
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+        FirebaseRecyclerOptions<person> options =
+                new FirebaseRecyclerOptions.Builder<person>()
+                        .setQuery(mbase, person.class)
+                        .build();
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        adapter = new personAdapter(options,itemClickListener);
+        // Connecting Adapter class with the Recycler view*/
+        recyclerView.setAdapter(adapter);
+        return view;
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    // Function to tell the app to stop getting
+    // data from database on stopping of the activity
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    public void bustrackmenu() {
+        Intent i=new Intent(getContext(),BusTrackMenu.class);
+        startActivity(i);
+    }
+}
