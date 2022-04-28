ed3b7a74e1fb8b8a87d8e6d8e7e02d1c47c5f8e9
Brilliantcanady <venkyrssvv2002@gmail.com>
2022-04-28 20:47:37 +0530

    commit_6

--- app/src/main/java/com/example/tae/User_map_page.java
+++ app/src/main/java/com/example/tae/User_map_page.java
@@ -1,12 +1,25 @@
 package com.example.tae;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.PermissionRequest;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Response;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class User_map_page extends Fragment {
 
@@ -15,6 +28,33 @@ public class User_map_page extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_map_page, container, false);
+
+        View view= inflater.inflate(R.layout.fragment_user_map_page, container, false);
+        checkMyPermission();
+        return view;
     }
+
+    private void checkMyPermission() {
+        Dexter.withContext(getContext().getApplicationContext()).withPermission (Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                //Toast.makeText(getContext().getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Intent intent= new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts(  "package",getContext().getPackageName(),"");
+                intent.setData(uri);
+                startActivity (intent);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(com.karumi.dexter.listener.PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }
+        ).check();
+}
 }
